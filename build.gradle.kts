f71aa6f057fa5f479a49dd12f758cb76cea49ef1
abdulrahman-b <abdulrahman-b0@hotmail.com>
2025-02-15 20:58:09 +0300

    Set root project description and version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,10 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+// Top-level build file where you can add configuration options common to all subprojects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.android.library) apply false
 }
+
+rootProject.description  = "A modern and stylish Hijri Date Picker for Android, built with Jetpack Compose and inspired by the Material 3 Date Picker."
+rootProject.version = "1.0.0-alpha01"
