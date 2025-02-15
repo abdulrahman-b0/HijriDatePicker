18349b8733b087a3f590742d2294f003a67aa489
abdulrahman-b <abdulrahman-b0@hotmail.com>
2025-02-15 21:28:59 +0300

    Removed serialization libs

--- hijri-date-picker/build.gradle.kts
+++ hijri-date-picker/build.gradle.kts
@@ -6,7 +6,6 @@ plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.vanniktechMavenPublish)
 }
 
@@ -43,7 +42,6 @@ android {
 dependencies {
 
     implementation(libs.hijrahdatetime)
-    implementation(libs.kotlin.serialization.protobuf)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.activity.compose)
