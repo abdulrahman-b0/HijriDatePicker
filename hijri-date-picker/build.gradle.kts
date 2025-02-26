99f7b21b96ab2abdb15934f840a1975329617776
abdulrahman-b <abdulrahman-b0@hotmail.com>
2025-02-26 20:10:25 +0300

    Remove custom Maven repository configuration
    The publishing block for a custom Maven repository was removed, streamlining the build script. This change ensures cleaner configuration and potentially reduces maintenance overhead.

--- hijri-date-picker/build.gradle.kts
+++ hijri-date-picker/build.gradle.kts
@@ -100,19 +100,6 @@ mavenPublishing {
     signAllPublications()
 }
 
-publishing {
-    repositories {
-        maven("https://maven.abdulrahman-b.com/releases") {
-            name = "Reposilite"
-            credentials {
-                this.username = globalGradleProperties.getProperty("reposilite.username")
-                this.password = globalGradleProperties.getProperty("reposilite.password")
-            }
-        }
-    }
-}
-
-
 private fun Project.configurePom(target: MavenPom) = with(target) {
     name = gradleProperties.getProperty("project.name")
     description = gradleProperties.getProperty("project.description")
