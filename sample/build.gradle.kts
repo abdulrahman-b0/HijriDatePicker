20d4161ec4643ba27a31b13b7fbe1485e188ef3d
Abdulrahman Bahamel <Abdulrahman-b0@hotmail.com>
2025-12-12 14:30:19 +0300

    Bump sample app targetSdk to 36 and migrate `kotlinOptions` to `compilerOptions`.

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -1,5 +1,8 @@
 @file:Suppress("UnstableApiUsage")
 
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -13,7 +16,7 @@ android {
     defaultConfig {
         applicationId = "com.abdulrahman_b.hijridatepicker"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
@@ -35,9 +38,11 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
+   kotlin {
+       compilerOptions {
+           jvmTarget = JvmTarget.JVM_17
+       }
+   }
     buildFeatures {
         compose = true
     }
